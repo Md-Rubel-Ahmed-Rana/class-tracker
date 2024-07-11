93a80064f6aa03f822b321c67690b742650ac0f0
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-07-12 01:58:38 +0600

    Adding new student page design and API integration done

--- client/src/components/add-new-student/index.tsx
+++ client/src/components/add-new-student/index.tsx
@@ -1,30 +1,11 @@
-"use client";
+import { postApi } from "@/apis";
+import { AppContext } from "@/context/AppProvider";
+import { IBatch } from "@/types/batch.type";
 import { INewStudent } from "@/types/newStudent.type";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useContext, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-
-const batches = [
-  {
-    id: "3467896",
-    name: "Cohod",
-    batchNo: "ADC0001",
-  },
-  {
-    id: "34678987t8g6",
-    name: "Cohod",
-    batchNo: "ADC0002",
-  },
-  {
-    id: "3467896fghedrt",
-    name: "Cohod",
-    batchNo: "ADC0003",
-  },
-  {
-    id: "3467896rutheio5uth",
-    name: "Cohod",
-    batchNo: "ADC0004",
-  },
-];
+import { toast } from "react-toastify";
 
 const AddNewStudentPage = () => {
   const {
@@ -32,18 +13,37 @@ const AddNewStudentPage = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<INewStudent>();
+  const { batches }: { batches: IBatch[] | [] } = useContext(AppContext);
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleAddNewStudent: SubmitHandler<INewStudent> = async (data) => {
+    setIsLoading(true);
     try {
-      console.log(data);
+      const result = await postApi("student/create-student", data);
+      if (result?.success === true) {
+        toast.success(result?.message);
+        const selectedBatch = batches.find(
+          (batch) => batch.batchNo === data.batchNo
+        );
+        router.push(
+          `/batches/details/${selectedBatch?.id}?name=${selectedBatch?.name}&batchNo=${selectedBatch?.name}`
+        );
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+      }
     } catch (error) {
       console.error("Error adding student:", error);
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-gray-50 p-8 rounded shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Add New Student</h2>
         <form onSubmit={handleSubmit(handleAddNewStudent)}>
           <div className="mb-4">
@@ -82,7 +82,7 @@ const AddNewStudentPage = () => {
               <option value="">Select a batch</option>
               {batches.map((batch) => (
                 <option key={batch.id} value={batch.batchNo}>
-                  <span>{batch.name}</span>|<span>{batch.batchNo}</span>
+                  {batch.batchNo}
                 </option>
               ))}
             </select>
@@ -93,10 +93,13 @@ const AddNewStudentPage = () => {
             )}
           </div>
           <button
+            disabled={isLoading}
             type="submit"
-            className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className={`${
+              isLoading ? "bg-gray-500" : "bg-blue-500 w-full hover:bg-blue-700"
+            } text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline`}
           >
-            Submit
+            {isLoading ? "Submitting student info..." : "Submit"}
           </button>
         </form>
       </div>
